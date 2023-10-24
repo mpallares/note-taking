0c6f0d0ad526b32f66623572074ab9db051a4dc2
Maria Pallarés <mariapallares@Marias-MBP.lan>
2023-10-24 07:10:23 +0100

    feature: add edit and delete tags modal

---
+++ src/components/EditTagsModal.tsx
@@ -0,0 +1,50 @@
+import { Button, Col, Form, Modal, Row, Stack } from 'react-bootstrap';
+import { Tag } from '../types';
+
+interface EditTagsModalProps {
+  show: boolean;
+  availableTags: Tag[];
+  handleClose: () => void;
+  onDeleteTag: (id: string) => void;
+  onUpdateTag: (id: string, label: string) => void;
+}
+export const EditTagsModal = ({
+  availableTags,
+  handleClose,
+  onDeleteTag,
+  onUpdateTag,
+  show,
+}: EditTagsModalProps) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Edit Tags</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Stack gap={2}>
+            {availableTags.map((tag) => (
+              <Row key={tag.id}>
+                <Col>
+                  <Form.Control
+                    type='text'
+                    value={tag.label}
+                    onChange={(e) => onUpdateTag(tag.id, e.target.value)}
+                  />
+                </Col>
+                <Col xs='auto'>
+                  <Button
+                    onClick={() => onDeleteTag(tag.id)}
+                    variant='outline-danger'
+                  >
+                    &times;
+                  </Button>
+                </Col>
+              </Row>
+            ))}
+          </Stack>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
